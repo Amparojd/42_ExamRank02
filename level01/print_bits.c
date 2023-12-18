e6890776332714cab1545de61306f1d9ec863c7f
Amparo Jiménez <amparo_jd@hotmail.com>
2023-12-18 21:08:06 +0100

    add level 00 and level 01

---
+++ level01/print_bits.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void	print_bits(unsigned char octet)
+{
+   int i = 8;
+
+   while (i--)
+   {
+        if (octet & (1 << i))
+            write(1, "1", 1);
+        else
+            write(1, "0", 1);
+   }
+}
