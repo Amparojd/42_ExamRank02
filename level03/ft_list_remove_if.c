22ed57affffd77641165434ca76ed0d25cbd3a67
ampjimen <ampjimen@student.42madrid.com>
2024-01-22 18:50:46 +0100

    list_remove modificado

--- level03/ft_list_remove_if.c
+++ level03/ft_list_remove_if.c
@@ -41,13 +41,13 @@ void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 
     if(cmp(lst_ptr->data, data_ref) == 0)
     {
-        *begin_list = list_ptr->next;
-        free(list_ptr);
+        *begin_list = lst_ptr->next;
+        free(lst_ptr);
         ft_list_remove_if(begin_list, data_ref, cmp);
     }
     else
     {
-        list_ptr = *begin_list;
-        ft_list_remove_if(&list_ptr->next, data_Ref, cmp);
+        lst_ptr = *begin_list;
+        ft_list_remove_if(&lst_ptr->next, data_ref, cmp);
     }
 }
